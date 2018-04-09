cc98e5ce8a110210b57347f8b2bf546afbc24f0e
Alejandro Sierra <gasierram@unal.edu.co>
2018-04-08 21:37:24 -0500

    crud ok

--- 1a_login_db/crud_database.sql
+++ 1a_login_db/crud_database.sql
@@ -1,87 +1,19 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS users;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE `users`.`tbl_user` (
-  `user_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(255) NULL,
-  `user_email` VARCHAR(255) NULL,
-  `user_password` VARCHAR(255) NULL,
-  PRIMARY KEY (`user_id`));
+-- ----------------------------
+-- Table structure for user
+    -- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+    `id` int(50) NOT NULL AUTO_INCREMENT,
+    `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL UNIQUE,
+    `email` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL UNIQUE,
+    `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
-
-USE `users`;
-DROP procedure IF EXISTS `sp_createUser`;
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
-    IN p_name VARCHAR(255),
-    IN p_email VARCHAR(255),
-    IN p_password VARCHAR(255)
-)
-BEGIN
-    if ( select exists (select 1 from tbl_user where user_email = p_email) ) THEN
-     
-        select 'Email Exists !!';
-     
-    ELSE
-     
-        insert into tbl_user
-        (
-            user_name,
-            user_email,
-            user_password
-        )
-        values
-        (
-            p_name,
-            p_email,
-            p_password
-        );
-     
-    END IF;
-END$$
-DELIMITER ;
-
-
-USE `users`;
-DROP procedure IF EXISTS `sp_AuthenticateUser`;
-
-DELIMITER $$
-USE `users`$$
-CREATE PROCEDURE `sp_AuthenticateUser` (
-IN p_email VARCHAR(255)
-)
-BEGIN
-
-     select * from tbl_user where user_email = p_email;
-
-END$$
-
-DELIMITER ;
-
-
-
-USE `users`;
-DROP procedure IF EXISTS `sp_GetAllItems`;
-
-DELIMITER $$
-USE `users`$$
-CREATE PROCEDURE `sp_GetAllItems` (
-)
-BEGIN
-    select * from tbl_user; 
-END$$
-
-DELIMITER ;
-
-USE `users`;
-DROP procedure IF EXISTS `sp_deleteUser`;
-
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_deleteUser`(
-    IN p_email VARCHAR(255)
-)
-BEGIN
-	DELETE FROM tbl_user where user_email = p_email;
-END$$
-DELIMITER ;
+SET FOREIGN_KEY_CHECKS = 1;
--- app.py
+++ app.py
@@ -1,207 +1,109 @@
-from flask import Flask, request 
-from flask_restful import Resource, Api, reqparse	
-from flask.ext.mysql import MySQL
-from werkzeug import generate_password_hash, check_password_hash
-from flask import flash, render_template, redirect, url_for, session
-import json
-#from module.database import Database
-app = Flask(__name__)
-api = Api(app)
+import os
+from pprint import pprint
 
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import json
+import json as simplejson
+#import simplejson
+from werkzeug.security import generate_password_hash, check_password_hash
+from flaskext.mysql import MySQL
 
-@app.route('/')
-def hello():
-    return 'Hello World! I have been seen'
-
+#project_root = os.path.dirname(__name__)
+#template_path = os.path.join(project_root)
 
 mysql = MySQL()
-#Conectando MySQL
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
-app.config['MYSQL_DATABASE_DB'] = 'users'
-app.config['MYSQL_DATABASE_HOST'] = '1a_login_db'
-#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app = Flask(__name__)
+#app = Flask(__name__,template_folder=template_path)
+# mysql configuratoin
+app.config['MYSQL_DATABASE_HOST']       = '1a_login_db'
+app.config['MYSQL_DATABASE_USER']       = 'root'
+app.config['MYSQL_DATABASE_PASSWORD']   = '1234'
+app.config['MYSQL_DATABASE_DB']         = 'users'
 mysql.init_app(app)
 
+@app.route('/')
+def main_world():
+    #return render_template('static/index.html')
+    return 'Hello World! I have been seen'
 
-#POST create
-class login(Resource):
-    def post(self):
-        try:
-            # Parse the arguments
+@app.route('/login',methods=['POST'])
+def signIn():
+    # open connection
 
-            parser = reqparse.RequestParser()
-            parser.add_argument('email', type=str, help='Email address for Authentication')
-            parser.add_argument('password', type=str, help='Password for Authentication')
-            args = parser.parse_args()
+    # # read request from UI
+    # _name   = request.form['name']
+    # _email  = request.form['email']
+    # _pass   = request.form['pass']
+    request_json     = request.get_json()
+    _name           = request_json.get('name')
+    _email           = request_json.get('email')
+    _pass           = request_json.get('pass')
+#    _hash_pass = generate_password_hash(_pass)
 
-            _userEmail = args['email']
-            _userPassword = args['password']
+    if _name and _email and _pass:
+        insert(_name,_email,_pass)
+        return json.dumps({'success':'Data Inserted '})
+    else:
+        return json.dumps({'error':'Enter the required fields'})
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.callproc('sp_AuthenticateUser',(_userEmail, ))
-            data = cursor.fetchall()
+def insert(user,email,password):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(
+        """INSERT INTO users.user (
+                username,
+                email,
+                password
+            ) 
+            VALUES (%s,%s,%s)""",(user,email,password))
+    conn.commit()
+    conn.close()
 
-            if(len(data)>0):
-                if(str(data[0][3])==_userPassword):
-                    conn.commit()
-                    return json.dumps({'status':200,'UserId':str(data[0][0])})
-                    #session['user'] = data[0][0]
-                    #return redirect('/')
-                else:
-                    return json.dumps({'status':100,'message':'Authentication failure'})
-            
-        except Exception as e:
-            return json.dumps({'error': str(e)})
+@app.route('/show')
+def show():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users.user")
+    data = cursor.fetchall()
+    dataList = []
+    if data is not None:
+        for item in data:
+            dataTempObj = {
+                'id'        : item[0],
+                'name'      : item[1],
+                'email'     : item[2],
+                'password'  : item[3]
+            }
+            dataList.append(dataTempObj)
+        return json.dumps(dataList)
+    else:
+        return 'error'
 
+@app.route('/update/<id>',methods=['POST'])
+def update(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    result = cursor.execute("UPDATE user SET username = %s, email = %s, password = %s WHERE id = %s",(request.form['username'],request.form['email'],request.form['password'],int(id)))
+    conn.commit()
+    conn.close()
+    if(result):
+        return json.dumps({'updated':'true'})
+    else:
+        return json.dumps({'updated':'false'})
 
-api.add_resource(login, '/login')
-
-#POST Create
-class CreateUser(Resource):
-    global cursor
-    def post(self): 
-        try: 
-            #parse the arguments
-            parser = reqparse.RequestParser()
-            parser.add_argument('email', type=str, help='Email address to create user')
-            parser.add_argument('password', type=str, help='Password to create user')
-            args = parser.parse_args()
-
-            _userEmail = args['email']
-            _userPassword = args['password']
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-
-            #_hashed_password = generate_password_hash(_userPassword)
-
-            cursor.callproc('sp_createUser',('',_userEmail,_userPassword))#_hashed_password))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-               # return {'Email': args['email'], 'Password': args['password']}
-                return json.dumps(args).replace("\'", '"')
-            else:
-                return json.dumps({'error':str(data[0])})    
-
-        except Exception as e: 
-            return json.dumps({'error': str(e)})
-        finally:
-            cursor.close()
-            conn.close()
-api.add_resource(CreateUser, '/CreateUser')
-
-#Delete User
-class DeleteUser(Resource):
-    global cursor
-    def post(self): 
-        try: 
-            #parse the arguments
-            parser = reqparse.RequestParser()
-            parser.add_argument('email', type=str, help='Email address to delete user')
-            #parser.add_argument('password', type=str, help='Password to delete user')
-            args = parser.parse_args()
-
-            _userEmail = args['email']
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-
-            #_hashed_password = generate_password_hash(_userPassword)
-
-            cursor.callproc('sp_deleteUser',(_userEmail,))#_hashed_password))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-               # return {'Email': args['email'], 'Password': args['password']}
-                return json.dumps({'Ha cerrado sesion el usuario': args}).strip('"\"')
-            else:
-                return json.dumps({'error':str(data[0])})    
-
-        except Exception as e: 
-            return json.dumps({'error': str(e)})
-        finally:
-            cursor.close()
-            conn.close()
-api.add_resource(DeleteUser, '/DeleteUser')
-
-#GET login
-class GetUsers(Resource):
-    def get(self):
-        try: 
-            # Parse the arguments
-            #parser = reqparse.RequestParser()
-            #parser.add_argument('email', type=str)
-            #args = parser.parse_args()
-
-            #_userEmail = args['email']
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.callproc('sp_GetAllItems',)
-            data = cursor.fetchall()
-
-            users_list=[]
-            for item in data:
-                i = {
-                    'id':item[0],
-                    'user':item[1],
-                    'email':item[2],
-                    'password':item[3]
-                }
-                users_list.append(i)
-
-            return {'StatusCode':'200','Items':users_list}
-
-        except Exception as e:
-            return {'error': str(e)}
-
-api.add_resource(GetUsers, '/GetUsers')
-
-#POST Update
-class UpdateUser(Resource):
-    global cursor
-    def post(self): 
-        try: 
-            #parse the arguments
-            parser = reqparse.RequestParser()
-            parser.add_argument('email', type=str, help='Email address to update user')
-            parser.add_argument('password', type=str, help='Password to update user')
-            args = parser.parse_args()
-
-            _userEmail = args['email']
-            _userPassword = args['password']
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-
-            #_hashed_password = generate_password_hash(_userPassword)
-
-            cursor.callproc('sp_updateUser',('',_userEmail,_userPassword))#_hashed_password))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-               # return {'Email': args['email'], 'Password': args['password']}
-                return json.dumps(args).strip("\\")
-            else:
-                return json.dumps({'error':str(data[0])})    
-
-        except Exception as e: 
-            return json.dumps({'error': str(e)})
-        finally:
-            cursor.close()
-            conn.close()
-api.add_resource(UpdateUser, '/UpdateUser')
-
-@app.route('/logout')
-def logout():
-    session.pop('user',None)
-    return redirect('/')
+@app.route('/delete/<id>')
+def delete(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    result = cursor.execute("DELETE FROM user WHERE id = %s",int(id))
+    conn.commit()
+    conn.close()
+    if(result):
+        return json.dumps({'delete':'success'})
+    else:
+        return json.dumps({'delete':'failure'})
 
 
 @app.errorhandler(404)
@@ -209,5 +111,4 @@ def page_not_found(error):
     return render_template('error.html')
 
 if __name__ == '__main__':
-    #app.run(debug=True,)
     app.run(host="0.0.0.0")
